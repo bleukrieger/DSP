1752dfb5d4bb19b832b83023d34d62a867302284
bleukrieger <sanjiv.pant@gmail.com>
2023-07-27 19:41:03 +0545

    Sound Engineering continued

--- aMinorTests.m
+++ aMinorTests.m
@@ -14,9 +14,14 @@ ch1dsfft = fftshift(abs(fft(ch1ds)));
 %subplot(2,1,2), plot(ch1dsfft);
 
 % basic digital filter LowPass
-num = [1 100];
-den = [100 1];
+num = [1];
+den = [1 1];
 zh = tf(num,den,fs);
 
-[y1, t1] = lsim(zh, ch1);
-plot(t1,y1)
+[y1, t1] = lsim(zh, ch1ds);
+%plot(t1,y1)
+
+y1fft = fftshift(abs(fft(y1)));
+figure()
+subplot(2,1,1), plot(ch1dsfft);
+subplot(2,1,2), plot(y1fft);
